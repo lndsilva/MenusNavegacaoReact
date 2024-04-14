5846ccc22f6eba74e56e59aa3f1f6cf8a4dd1ec4
lndsilva <lndsilva@hotmail.com>
2024-04-14 19:02:41 -0300

    drawer layout button openDrawer

--- src/views/TelaD.jsx
+++ src/views/TelaD.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import TextoCentral from "../componentes/TextoCentral";
 
 export default props => {
     return (
-        <View style={{flex:1}}>
-            <TextoCentral corFundo='#135D66'>TelaC!</TextoCentral>
+        <View style={{flex:1}}>   
+                <View style={{flexDirection:'row', justifyContent:'space-around'}}>
+                    <Button 
+                        title="Abrir"
+                        onPress={()=>props.navigation.openDrawer()}
+                    />
+                </View>          
+            
+                <TextoCentral corFundo='#6196A6'>
+                    TelaD!
+               
+                </TextoCentral>
+                
+            
         </View>
     )
 }

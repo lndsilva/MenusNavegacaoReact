0ef40dac3f26980fa55ca9792a77408aa51ac17c
lndsilva <lndsilva@hotmail.com>
2024-04-14 18:05:58 -0300

    drawer layout

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -1,15 +1,18 @@
+import 'react-native-gesture-handler';
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import Drawer from "./Drawer";
+//import Tab from "./Tab";
 //import Stack from "./Stack";
-import Tab from "./Tab";
 
 export default props =>{
     return (
         <SafeAreaView style={{flex:1}}>
             <NavigationContainer>
-                {/*<Stack />*/}
-                <Tab/>
+                {/*<Tab/>
+                  <Stack />*/}
+                <Drawer />
             </NavigationContainer>
             
         </SafeAreaView>

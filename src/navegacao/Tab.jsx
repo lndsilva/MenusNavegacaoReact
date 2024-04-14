8bce01c0893269e4c815d105a114ac6189f3d6a1
lndsilva <lndsilva@hotmail.com>
2024-04-14 20:42:15 -0300

    drawer layout icons

--- src/navegacao/Tab.jsx
+++ src/navegacao/Tab.jsx
@@ -4,17 +4,23 @@ import TelaA from "../views/TelaA";
 import TelaB from "../views/TelaB";
 import TelaC from "../views/TelaC";
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const Tab = createBottomTabNavigator()
 
 export default props => (    
-    <Tab.Navigator screenOptions={({})=>(
+    <Tab.Navigator screenOptions={({route})=>(
         {
             tabBarActiveTintColor: "red",
             tabBarInactiveTintColor: 'blue',
-            tabBarLabelStyle: { fontSize: 30}
+            tabBarLabelStyle: { fontSize: 30},
+            
         }
+        
     )} initialRouteName="TelaA"
+
+    
         
     >
         <Tab.Screen name="TelaA" component={TelaA}/>
--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -2,17 +2,17 @@ import 'react-native-gesture-handler';
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import Drawer from "./Drawer";
-//import Tab from "./Tab";
+//import Drawer from "./Drawer";
+import Tab from "./Tab";
 //import Stack from "./Stack";
 
 export default props =>{
     return (
         <SafeAreaView style={{flex:1}}>
             <NavigationContainer>
-                {/*<Tab/>
-                  <Stack />*/}
-                <Drawer />
+                <Tab/>
+                {/* <Stack /> */}
+                {/* <Drawer /> */}
             </NavigationContainer>
             
         </SafeAreaView>
